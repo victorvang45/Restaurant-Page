7a3c934f6292f65a4d41201b4928add1b9595bf1
Victor Vang <victorvang456@gmail.com>
2025-05-04 23:50:59 -0500

    Add navagation between pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
 import "./styles.css";
-
 import { createHome } from "./home";
 import { createMenu } from "./menu";
 import { createAbout } from "./about";
-console.log("Hello World!");
 
+
+function setActiveButton(button) {
+    const buttons = document.querySelectorAll(".button-nav");
+
+
+    buttons.forEach((button) => {
+        if (button !== this) {
+            button.classList.remove("active");
+        }
+    });
+
+    button.classList.add("active");
+}
+
+
+
+//initialize
 
 const contentDiv = document.getElementById('content');
 
-createMenu(contentDiv);
+const homeButton = document.getElementById('home-btn');
+homeButton.classList.add("button-nav");
+homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    contentDiv.replaceChildren();
+    setActiveButton(homeButton);
+    createHome(contentDiv);
+  });
+
+const menuButton = document.getElementById('menu-btn');
+menuButton.classList.add("button-nav");
+menuButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    contentDiv.replaceChildren();
+    setActiveButton(menuButton);
+    createMenu(contentDiv);
+  });
+
+const aboutButton = document.getElementById('about-btn');
+aboutButton.classList.add("button-nav");
+aboutButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    contentDiv.replaceChildren();
+    setActiveButton(aboutButton);
+    createAbout(contentDiv);
+  });
+
+setActiveButton(document.querySelector(".button-nav"));
+createHome(contentDiv);
+
 
 
 
